5eb614053ee83c78bb3ef5f60ae1dc73f787abd0
James Saunders <james@saunders-family.net>
2019-01-11 21:44:47 +0000

    Improve prep_file.py.

--- prep_file.py
+++ prep_file.py
@@ -2,59 +2,59 @@
 
 import os
 import sys
-import zipfile
 import hashlib
 import subprocess
 
-
-OUTPUT_FOLDER = "OUTPUT"
-
+BINARIES_FOLDER = "malwares/Binaries"
+SOURCE_FOLDER = "malwares/Source/Original"
 
 def _help():
-    print("Please run with '%s filename'." % sys.argv[0])
+    print("Please run with '%s {input folder} {output filename} {s|b}'." % sys.argv[0])
     return
 
-def _Do(file_path):
-    if not os.path.isfile(file_path):
+def _Do(input_folder, name, type):
+
+    if type == 'b':
+        path = BINARIES_FOLDER + "/" + name
+    elif type == 's':
+        path = SOURCE_FOLDER + "/" + name
+    else:
+        print("Type Source or Binary not valid ('s' or 'b').")
+        sys.exit(1)
+
+    if not os.path.isdir(input_folder):
         _help()
-        print("Seems like '%s' is not a file." % file_path)
+        print("Seems like '%s' is not a input_folder." % input_folder)
         sys.exit(1)
 
     try:
-        os.mkdir(OUTPUT_FOLDER)
+        os.makedirs(path)
     except OSError:
         print("Folder exists. Please remove it before continuing.")
         sys.exit(1)
 
-    if "\\" in file_path:
-        filename = file_path.split("\\")[:-1]
-    elif "/" in file_path:
-        filename = file_path.split("/")[:-1]
-    else:
-        filename = file_path
-
     # Create ZIP Archive:
     try:
-        rc = subprocess.call(['7z', 'a', '-pinfected', '-y', '%s/%s.zip' % (OUTPUT_FOLDER, filename)] + [file_path])
+        subprocess.call(['7z', 'a', '-pinfected', '-y', '%s/%s.zip' % (path, name), '%s/*' % input_folder])
     except:
         print("Seems like you don't have 7z in your path. Please install or add with:\n\tbrew install 7zip #(OSX)\n\tsudo apt-get install p7zip-full #(Linux)")
         sys.exit(1)
 
-    compressed_path = '%s/%s.zip' % (OUTPUT_FOLDER, filename)
+    compressed_path = '%s/%s.zip' % (path, name)
     print("Created ZIP Archive.")
     md5sum = hashlib.md5(open(compressed_path, 'rb').read()).hexdigest()
     sha1sum = hashlib.sha1(open(compressed_path, 'rb').read()).hexdigest()
-    open("%s/%s.md5" % (OUTPUT_FOLDER, filename), 'w').write(md5sum)
-    open("%s/%s.sha" % (OUTPUT_FOLDER, filename), 'w').write(sha1sum)
-    open("%s/%s.pass" % (OUTPUT_FOLDER, filename), 'w').write("infected")
+    open("%s/%s.md5" % (path, name), 'w').write(md5sum)
+    open("%s/%s.sha" % (path, name), 'w').write(sha1sum)
+    open("%s/%s.pass" % (path, name), 'w').write("infected")
     return True
 
 
 if __name__ == "__main__":
-    if len(sys.argv) != 2:
+    if len(sys.argv) != 4:
         _help()
         sys.exit(1)
-    _Do(sys.argv[1])
-    print("Please don't forget to add details to 'conf/maldb.db'.")
-    print("Thanks for helping us get this accessible to everyone.")
-    print("")
+
+    _Do(sys.argv[1], sys.argv[2], sys.argv[3])
+    print("Please don't forget to add details to 'conf/maldb.db'.\n")
+    print("Thanks for helping us get this accessible to everyone.\n")
